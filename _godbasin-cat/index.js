e0cffddf13ab2ffe83fc258f679fae2419581cfa
luo0412 <1095847440@qq.com>
2022-06-08 10:11:46 +0000

    adjust

---
+++ _godbasin-cat/index.js
@@ -0,0 +1,5 @@
+// https://github.com/godbasin/front-end-playground/blob/sourcecode/docs/.vuepress/theme/components/KittyTop.vue
+import { createApp } from 'vue'
+import App from './index.vue'
+
+createApp(App).mount('#app')
--- mpa.config.js
+++ mpa.config.js
@@ -21,5 +21,7 @@ module.exports = {
 
     // entry: './_use-event-diff/*.js',
 
-    entry: './_vue-virtual-scroller/*.js',
+    // entry: './_vue-virtual-scroller/*.js',
+
+    entry: './_godbasin-cat/*.js',
 }
